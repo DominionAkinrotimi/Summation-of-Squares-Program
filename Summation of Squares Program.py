bf8d835a5d10b0ea3b1f55f2ce87f8df3d760c1d
Akinrotimi Dominion <134118389+DominionAkinrotimi@users.noreply.github.com>
2023-12-26 18:02:32 +0100

    Add files via upload

---
+++ Summation of Squares Program.py
@@ -0,0 +1,28 @@
+'''  QUESTION
+        n   
+  y =    E   (x^2) 
+        x=1  
+
+        Note: from the above formula, the letter E is used to represent the summation sign
+        Represent the above formula with a program in python
+'''
+sum = 0
+n = int(input("last number: "))
+for x in range(1,n):
+    print(f"x: {x}")
+    y = (x**2)
+    sum = sum + y
+    print(f"{x}^2 = {y}" )
+y = sum
+print("")
+print(f"y = {y}" )
+
+
+
+
+
+
+
+
+
+
